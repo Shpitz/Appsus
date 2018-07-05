3006a249926f9dd0bf06fefaa47b360ee1a2302f
Din_Hazan <dinhazan7@gmail.com>
2018-07-05 17:19:10 +0300

    merge fixing

--- js/routes.js
+++ js/routes.js
@@ -1,5 +1,5 @@
 import appsusApp from './pages/appsus-app-cmp.js'
-import misterKeep from './pages/notes-app-cmp'
+import misterKeep from './pages/notes-app-cmp.js'
 import editNote from './cmps/notes-cmps/edit-note-cmp.js'
 import misterEmail from './pages/email-app-cmp.js'
 // import about from './pages/about.js'
