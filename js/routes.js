2db567710a0ce47d5044a830509c1dd666ed82bf
Din_Hazan <dinhazan7@gmail.com>
2018-07-08 22:22:37 +0300

    routs change

--- js/routes.js
+++ js/routes.js
@@ -1,16 +1,13 @@
 import appsusApp from './pages/appsus-app-cmp.js'
 import misterKeep from './pages/notes-app-cmp.js'
-import editNote from './cmps/notes-cmps/edit-note-cmp.js'
 import misterEmail from './pages/email-app-cmp.js'
 // import about from './pages/about.js'
 
 
 const routes = [
-    {path: '/apps', component: appsusApp},
+    {path: '/', component: appsusApp},
     {path: '/email', component: misterEmail},
     {path: '/notes', component: misterKeep},
-    {path: '/notes/:id', component: editNote},
-    // {path: '/about', component: about},
   ];
 
 Vue.use(VueRouter);
